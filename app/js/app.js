a2004a8140cd635fb4cf76a46fd63b1211ea8cb8
Outpox <guillaumeoutpox@gmail.com>
2015-06-17 00:39:42 +0200

    Many things, mostly backend connexion + new features

--- app/js/app.js
+++ app/js/app.js
@@ -1,72 +1,38 @@
 (function () {
-    angular.module('oc4fe', ['ngMaterial', 'ngRoute'])
+    angular.module('oc4fe', ['ngMaterial', 'ngRoute', 'ngResource'])
         .factory('Data', function () {
             return {
                 payeur: '',
-                date: new Date(),
-                loading: false
+                date: new Date().setHours(0, 0, 0, 0),
+                loading: false,
+                presents: [],
+                nbCafe: 0
             }
         })
-        .factory('People', function () {
-            var date1 = new Date();
-            var date2 = new Date();
-            date1.setDate(date1.getDate() - 1);
-            date2.setDate(date2.getDate() - 5);
-
-            return [
-                {
-                    id: 1,
-                    prenom: 'Guillaume',
-                    img: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAIAAAAAJGNjYjA0MzNkLTllYjEtNDU1OC04YTA2LTA4NjEzNDIxOWUwYw.jpg',
-                    dernierCafe: new Date(),
-                    joursDepuisDernierCafe: 0,
-                    payeAjd: false,
-                    present: false,
-                    sucre: true,
-                    touillette: true,
-                    vacances: true,
-                    cachee: false
-                },
-                {
-                    id: 2,
-                    prenom: 'Dahyun',
-                    img: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/08a/100/0427ba0.jpg',
-                    dernierCafe: date1,
-                    joursDepuisDernierCafe: 0,
-                    payeAjd: false,
-                    present: false,
-                    sucre: true,
-                    touillette: true,
-                    vacances: false,
-                    cachee: false
-                },
-                {
-                    id: 3,
-                    prenom: 'Jérôme',
-                    img: 'img/account-circle.svg',
-                    dernierCafe: date2,
-                    joursDepuisDernierCafe: 0,
-                    payeAjd: false,
-                    present: false,
-                    sucre: true,
-                    touillette: true,
-                    vacances: false,
-                    cachee: false
-                },
-                {
-                    id: 4,
-                    prenom: 'Eric',
-                    img: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/000/0da/227/3dcd144.jpg',
-                    dernierCafe: new Date(),
-                    joursDepuisDernierCafe: 0,
-                    payeAjd: false,
-                    present: false,
-                    sucre: true,
-                    touillette: true,
-                    vacances: false,
-                    cachee: false
+        .factory('People', ['$resource', function ($resource) {
+            return $resource('http://localhost:3000/users/:id', null, {
+                update: {
+                    method: 'PUT' // this method issues a PUT request
                 }
-            ]
+            });
+        }])
+        .factory('Tools', function () {
+            return {
+                jourDiff: function (date1) {
+                    var date2 = new Date().setHours(0, 0, 0, 0);
+                    var diff = {};                    // Initialisation du retour
+                    var tmp = date2 - date1;
+                    tmp = Math.floor(tmp / 1000);             // Nombre de secondes entre les 2 dates
+                    diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+                    tmp = Math.floor((tmp - diff.sec) / 60);    // Nombre de minutes (partie entière)
+                    diff.min = tmp % 60;                    // Extraction du nombre de minutes
+                    tmp = Math.floor((tmp - diff.min) / 60);    // Nombre d'heures (entières)
+                    diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+                    tmp = Math.floor((tmp - diff.hour) / 24);   // Nombre de jours restants
+                    diff.day = tmp;
+                    return diff.day;
+                }
+            }
         })
         .controller('toolbarCtrl', ['$scope', '$location', 'People', 'Data', '$mdBottomSheet', function ($scope, $location, People, Data, $mdBottomSheet) {
             $scope.people = People;
@@ -94,73 +60,101 @@
             $scope.editUser = function () {
                 $mdBottomSheet.hide();
                 $location.path('/userlist');
+            };
+            $scope.userInfo = function () {
+                $mdBottomSheet.hide();
+                $location.path('/userinfolist');
             }
         }])
-        .controller('listCtrl', ['$scope', '$location', 'People', 'Data', function ($scope, $location, People, Data) {
-            function dateDiff(date1, date2) {
-                var diff = {};                    // Initialisation du retour
-                var tmp = date2 - date1;
-                tmp = Math.floor(tmp / 1000);             // Nombre de secondes entre les 2 dates
-                diff.sec = tmp % 60;                    // Extraction du nombre de secondes
-                tmp = Math.floor((tmp - diff.sec) / 60);    // Nombre de minutes (partie entière)
-                diff.min = tmp % 60;                    // Extraction du nombre de minutes
-                tmp = Math.floor((tmp - diff.min) / 60);    // Nombre d'heures (entières)
-                diff.hour = tmp % 24;                   // Extraction du nombre d'heures
-                tmp = Math.floor((tmp - diff.hour) / 24);   // Nombre de jours restants
-                diff.day = tmp;
-                return diff.day;
-            }
+        .controller('listCtrl', ['$scope', '$location', 'People', 'Data', 'Tools', function ($scope, $location, People, Data, Tools) {
+            var tools = Tools;
+            People.query(function (data) {
+                $scope.people = data;
+                for (var p in $scope.people) {
+                    $scope.people[p].joursDepuisDernierCafe = tools.jourDiff(new Date($scope.people[p].dernierCafe));
+                }
+            });
 
-            $scope.people = People;
             $scope.Data = Data;
 
             $scope.presents = function (person) {
                 if (person.vacances) return;
-                person.payeAjd = true;
-                person.present = true;
                 $scope.Data.payeur = person;
                 $location.path('/presents');
             };
-
-            for (var p in $scope.people) {
-                $scope.people[p].joursDepuisDernierCafe = dateDiff($scope.people[p].dernierCafe, new Date());
-            }
         }])
         .controller('presentsCtrl', ['$scope', '$location', 'People', 'Data', function ($scope, $location, People, Data) {
-            $scope.people = People;
+            People.query(function (data) {
+                $scope.people = data;
+            });
             $scope.data = Data;
+
+            $scope.checked = function (person) {
+                return (person.id === $scope.data.payeur.id);
+            };
+
             $scope.selectAll = function () {
                 for (var p in $scope.people) {
                     $scope.people[p].present = (!$scope.people[p].cachee && !$scope.people[p].vacances);
                 }
             };
             $scope.goToSucre = function () {
-                $location.path('/sucre');
-            }
+                $scope.data.nbCafe = 0;
+                $scope.people.forEach(function (user) {
+                    if (user.present) {
+                        $scope.data.nbCafe++;
+                        $scope.data.presents.push(user);
+                        if (user.id !== $scope.data.payeur.id) {
+                            var p = People.query({id: user.id}, function () {
+                                var us = p[0];
+                                us.cafeRecus++;
+                                us.dernierCafe = new Date().setHours(0, 0, 0, 0);
+                                us.$update({id: us.id});
+                            });
+                        }
+                    }
+                });
+                var p = People.query({id: $scope.data.payeur.id}, function () {
+                    var us = p[0];
+                    us.cafePayes += $scope.data.nbCafe;
+                    us.$update({id: us.id}, function () {
+                        $location.path('/sucre');
+                    });
+                });
+            };
         }])
-        .controller('sucreCtrl', ['$scope', 'People', 'Data', function ($scope, People, Data) {
-            $scope.people = People;
+        .controller('sucreCtrl', ['$scope', 'Data', function ($scope, Data) {
             $scope.data = Data;
             $scope.data.payeur.dernierCafe = new Date();
-            $scope.qteSucre = $scope.qteCafe = $scope.qteTouillette = 0;
-            for (var p in $scope.people) {
-                if ($scope.people[p].sucre && $scope.people[p].present)
-                    $scope.qteSucre++;
-                if ($scope.people[p].touillette && $scope.people[p].present)
-                    $scope.qteTouillette++;
-                if ($scope.people[p].present)
-                    $scope.qteCafe++;
-            }
+            $scope.data.qteSucre = $scope.data.qteTouillette = 0;
+            $scope.data.presents.forEach(function (user) {
+                if (user.sucre) $scope.data.qteSucre++;
+                if (user.touillette) $scope.data.qteTouillette++;
+            });
+        }])
+        .controller('userCtrl', ['$scope', '$routeParams', 'People', 'Tools', function ($scope, $routeParams, People, Tools) {
+            var tools = Tools;
+            $scope.$route = $routeParams;
+            var user = People.query({id: Number($scope.$route.id)}, function () {
+                $scope.person = user[0];
+                $scope.person.joursDepuisDernierCafe = tools.jourDiff(new Date($scope.person.dernierCafe));
+                $scope.person.textSucre = $scope.person.sucre ? "Prend du sucre" : "Ne prend pas de sucre";
+                $scope.person.textTouillette = $scope.person.touillette ? "Prend une touillette" : "Ne prend pas de touillette";
+                $scope.person.textVacances = $scope.person.vacances ? "Est en vacances" : "N'est pas en vacances";
+                $scope.person.textCachee = $scope.person.cachee ? "Est cachée de la liste" : "Est visible dans la liste";
+            });
         }])
         .controller('newUserCtrl', ['$scope', 'People', 'Data', '$location', function ($scope, People, Data, $location) {
-            $scope.people = People;
+            People.query(function (data) {
+                $scope.people = data;
+            });
             $scope.data = Data;
-            var person = {};
             $scope.submit = function () {
+                var person = new People();
                 person.id = $scope.people.length + 1;
                 person.prenom = $scope.prenom;
                 person.img = $scope.img || 'img/account-circle.svg';
-                person.dernierCafe = new Date();
+                person.dernierCafe = new Date().setHours(0, 0, 0, 0);
                 person.joursDepuisDernierCafe = 0;
                 person.payeAjd = false;
                 person.present = false;
@@ -168,12 +162,17 @@
                 person.touillette = $scope.touillette;
                 person.vacances = false;
                 person.cachee = false;
-                $scope.people.push(person);
-                $location.path('/');
+                person.cafePayes = 0;
+                person.cafeRecus = 0;
+                person.$save(function (resp, respHeader) {
+                    $location.path('/');
+                });
             }
         }])
         .controller('userListCtrl', ['$scope', 'People', 'Data', '$location', function ($scope, People, Data, $location) {
-            $scope.people = People;
+            People.query(function (data) {
+                $scope.people = data;
+            });
             $scope.data = Data;
 
             for (var p in $scope.people) {
@@ -192,36 +191,44 @@
                 }
             }
 
+            $scope.userInfo = function (id) {
+                $location.path('/user/' + id);
+            };
             $scope.editUser = function (id) {
                 $location.path('edituser/' + id);
-            }
+            };
         }])
-        .controller('editUserCtrl', ['$scope', 'People', 'Data', '$location', '$routeParams', function ($scope, People, Data, $location, $route) {
-            $scope.people = People;
-            $scope.data = Data;
-            $scope.$route = $route;
-            var index = -1;
-            for (var i = 0, len = $scope.people.length; i < len; i++) {
-                if ($scope.people[i].id === Number($scope.$route.id)) {
-                    index = i;
-                    break;
+        .controller('editUserCtrl', ['$scope', 'People', 'Data', '$location', '$routeParams', function ($scope, People, Data, $location, $routeParams) {
+            $scope.$route = $routeParams;
+            var user = People.query({id: Number($scope.$route.id)}, function () {
+                $scope.people = user[0];
+                $scope.data = Data;
+                $scope.prenom = $scope.people.prenom;
+                $scope.sucre = $scope.people.sucre;
+                $scope.touillette = $scope.people.touillette;
+                $scope.vacances = $scope.people.vacances;
+                $scope.cachee = $scope.people.cachee;
+                $scope.img = $scope.people.img;
+                $scope.cafeRecus = $scope.people.cafeRecus;
+                $scope.cafePayes = $scope.people.cafePayes;
+
+                $scope.submit = function () {
+                    $scope.people.prenom = $scope.prenom;
+                    $scope.people.sucre = $scope.sucre;
+                    $scope.people.touillette = $scope.touillette;
+                    $scope.people.vacances = $scope.vacances;
+                    $scope.people.cachee = $scope.cachee;
+                    $scope.people.img = $scope.img || 'img/account-circle.svg';
+                    $scope.people.cafePayes = $scope.cafePayes;
+                    $scope.people.cafeRecus = $scope.cafeRecus;
+
+                    $scope.people.$update({id: $scope.people.id}, function (resp, respHeader) {
+                        if (resp.message === "OK") {
+                            $location.path('/');
+                        }
+                    });
                 }
-            }
-            $scope.prenom = $scope.people[index].prenom;
-            $scope.sucre = $scope.people[index].sucre;
-            $scope.touillette = $scope.people[index].touillette;
-            $scope.vacances = $scope.people[index].vacances;
-            $scope.cachee = $scope.people[index].cachee;
-            $scope.img = $scope.people[index].img;
-            $scope.submit = function () {
-                $scope.people[index].prenom = $scope.prenom;
-                $scope.people[index].sucre = $scope.sucre;
-                $scope.people[index].touillette = $scope.touillette;
-                $scope.people[index].vacances = $scope.vacances;
-                $scope.people[index].cachee = $scope.cachee;
-                $scope.people[index].img = $scope.img || 'img/account-circle.svg';
-                $location.path('/');
-            }
+            });
         }])
         .config(['$routeProvider', '$locationProvider', '$mdThemingProvider', function ($routeProvider, $locationProvider, $mdThemingProvider) {
             $mdThemingProvider.theme('default').primaryPalette('brown');
@@ -238,12 +245,20 @@
                     templateUrl: 'view/sucre.html',
                     controller: 'sucreCtrl'
                 })
+                .when('/user/:id', {
+                    templateUrl: 'view/user.html',
+                    controller: 'userCtrl'
+                })
                 .when('/newuser', {
                     templateUrl: 'view/newuser.html',
                     controller: 'newUserCtrl'
                 })
                 .when('/userlist', {
                     templateUrl: 'view/userlist.html',
+                    controller: 'userListCtrl'
+                })
+                .when('/userinfolist', {
+                    templateUrl: 'view/userinfolist.html',
                     controller: 'userListCtrl'
                 })
                 .when('/edituser/:id', {
